11b43384783aa78ffb5c654f95fd6baccf4b35b2
arushev <deso91@abv.bg>
2017-03-25 23:31:30 +0200

    Added cracking on bricks

--- mega-tank-2017/scripts/bricks.js
+++ mega-tank-2017/scripts/bricks.js
@@ -24,7 +24,15 @@ function getBricks(initialPositionX, initialPositionY) {
         },
 
         draw: function() {
+            if(health >= 75){
             drawRotatingImg(context, bricksPic, positionX, positionY, 0, 24, 24);
+            } else if(health >= 50){
+            drawRotatingImg(context, littleBrokenBricksPic, positionX, positionY, 0, 24, 24);
+            } else if (health >= 25){
+            drawRotatingImg(context, mediumBrokenBricksPic, positionX, positionY, 0, 24, 24);
+            } else {
+            drawRotatingImg(context, brokenBricksPic, positionX, positionY, 0, 24, 24);
+            }
         },
 
         onColide: function(otherObject) {
--- mega-tank-2017/scripts/image_loading.js
+++ mega-tank-2017/scripts/image_loading.js
@@ -1,6 +1,10 @@
 var tankPic = document.createElement('img');
 var cannonPic = document.createElement('img');
 var bricksPic = document.createElement('img');
+var littleBrokenBricksPic = document.createElement('img');
+var mediumBrokenBricksPic = document.createElement('img');
+var bricksPic = document.createElement('img');
+var brokenBricksPic = document.createElement('img');
 var rockPic = document.createElement('img');
 
 var picsToLoad = 0; // set default to 0
@@ -12,6 +16,9 @@ function loadImg(imgElem, fileName) {
 function loadGraphics() {
     let imgList = [
         { name: bricksPic, file: "bricks.png" },
+        { name: littleBrokenBricksPic, file: "little-broken-bricks.png" },
+        { name: mediumBrokenBricksPic, file: "medium-broken-bricks.png" },
+        { name: brokenBricksPic, file: "broken-bricks.png" },
         { name: tankPic, file: "tank.png" },
         { name: cannonPic, file: "cannon.png" },
         { name: rockPic, file: "rock.png"}
