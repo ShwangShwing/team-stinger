7f748a36cd5d823ff32d32609f1b888b06d2bff5
arushev <deso91@abv.bg>
2017-03-29 23:29:22 +0300

    again changed :palm_tree:

--- mega-tank-2017/scripts/game-engine.js
+++ mega-tank-2017/scripts/game-engine.js
@@ -153,7 +153,7 @@ function getGameEngine(gameCanvas) {
             // ];
 
             const enemyTurrets = [
-                getTurret(900, 100, 50, launchShell, 90, -0.02, 6),
+                getTurret(1200, 100, 50, launchShell, 90, -0.02, 6),
                 getTurret(825, 375, 50, launchShell, 25, 0.03, 6),
                 getTurret(250, 30, 50, launchShell, 90, -0.03, 6),
                 getTurret(100, 450, 50, launchShell, 90, 0.02, 6),
@@ -163,7 +163,7 @@ function getGameEngine(gameCanvas) {
 
             const enemyAntitankGuns = [getEnemyAntitankGun(1250, 450, 50, launchShell, Math.random() * Math.PI * 2, Math.random() * 0.08 - 0.04, playerTank),
                 getEnemyAntitankGun(695, 45, 50, launchShell, Math.random() * Math.PI * 2, Math.random() * 0.08 - 0.04, playerTank),
-                getEnemyAntitankGun(1200, 100, 50, launchShell, Math.random() * Math.PI * 2, Math.random() * 0.08 - 0.04, playerTank)
+                getEnemyAntitankGun(900, 100, 50, launchShell, Math.random() * Math.PI * 2, Math.random() * 0.08 - 0.04, playerTank)
             ];
 
             fieldObjects.push(topFieldBorder,
